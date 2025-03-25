1ad301a6e595e05e3b16581f5ee1559ff4ff15d7
Bhoomika <bhoomigupta803@gmail.com>
2025-03-25 15:34:48 -0400

    create classes

---
+++ src/enginedriver/Room.java
@@ -0,0 +1,17 @@
+package enginedriver;
+
+public class Room {
+  private String roomName;
+  private String description;
+  private int roomNumber;
+  private int N;
+  private int S;
+  private int E;
+  private int W;
+  private Puzzle puzzle;
+  private Monster monster;
+  private Item item;
+  private Fixture fixture;
+  private String picture;
+
+}
