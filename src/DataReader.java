1576450a5fb7e01c64e5ad67dfb3a4c27ecbee59
Harrison Pham <hpham1997@gmail.com>
2025-03-26 13:21:10 -0400

    add tests for DataReader

--- src/DataReader.java
+++ src/DataReader.java
@@ -4,15 +4,16 @@ import java.io.File;
 import java.io.IOException;
 
 import engineDriver.GameInfo;
+import engineDriver.Room;
 
 public class DataReader {
 
   public static void main(String[] args) throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
 
-    String hallwayjson = "C:/Users/hpham/Desktop/cs5004/hw/AdventureGame/Adventure_Game_5004/src/data/simple_hallway.json";
+    String hallwayjson = "src/data/simple_hallway.json";
     GameInfo game = objectMapper.readValue(new File(hallwayjson), GameInfo.class);
     System.out.println(game.getName());
-    System.out.println(game.getRooms());
+
   }
 }
---
+++ test/engineDriver/GameInfoTest.java
@@ -0,0 +1,68 @@
+package engineDriver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GameInfoTest {
+
+  GameInfo gameInfo;
+  List<Room> rooms;
+  Room r1;
+
+  @BeforeEach
+  void setUp() throws IOException {
+    String testJson = "src/data/museum.json";
+    ObjectMapper objectMapper = new ObjectMapper();
+    gameInfo = objectMapper.readValue(new File(testJson), GameInfo.class);
+  }
+
+  @Test
+  void testGetRooms() {
+    List<Room> rooms = gameInfo.getRooms();
+    Room entrance = rooms.getFirst();
+    assertEquals(1, entrance.getRoomNumber());
+    assertEquals("Museum Entrance", entrance.getName());
+    assertEquals("Turnstile", entrance.getPuzzle());
+    assertEquals(-2, entrance.getNorth());
+    assertNull(entrance.getMonster());
+  }
+
+  @Test
+  void testGetItems() {
+    List<Item> items = gameInfo.getItems();
+    Item ticket = items.getFirst();
+    assertEquals("Ticket", ticket.getName());
+    assertEquals(1, ticket.getWeight());
+    assertEquals("A complimentary museum ticket. It says ADMIT ONE, pwd = Align.",
+                 ticket.getDescription());
+  }
+
+  @Test
+  void testGetFixtures() {
+    List<Fixture> fixtures = gameInfo.getFixtures();
+    Fixture computer = fixtures.getFirst();
+    assertEquals("Computer", computer.getName());
+    assertEquals(1000, computer.getWeight());
+  }
+
+  @Test
+  void testGetPuzzle() {
+    List<Puzzle> puzzles = gameInfo.getPuzzles();
+    Puzzle password = puzzles.get(1);
+    assertEquals("PASSWORD", password.getName());
+    assertTrue(password.isActive());
+    assertTrue(password.AffectsTarget());
+    assertFalse(password.AffectsPlayer());
+  }
+}
