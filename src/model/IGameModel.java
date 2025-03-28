451d97ac9ff50ed38d6ecea907fed180fa6b8f80
Bhoomika <bhoomigupta803@gmail.com>
2025-03-28 14:40:26 -0400

    fix interface for model

--- src/model/IGameModel.java
+++ src/model/IGameModel.java
@@ -1,12 +1,12 @@
 package model;
 
 public interface IGameModel {
-  void move(String direction);
+  String move(String direction);
   String checkInventory();
   String look();
   String useItem(String itemName);
   String takeItem(String itemName);
   String dropItem(String itemName);
-  void examine(String object);
+  String examine(String object);
   String answer(String answer);
 }
