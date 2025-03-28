6e6f4b1e3979b9b6dd615f0c6cc936cc1963a7f6
Tony Hsu <hsutai.c@gmail.com>
2025-03-28 19:24:26 -0400

    added comments to all controller/command classes

--- src/controller/GameCommandFinder.java
+++ src/controller/GameCommandFinder.java
@@ -3,16 +3,38 @@ package controller;
 import java.util.Arrays;
 import java.util.List;
 
+import commands.AnswerCommand;
+import commands.DropCommand;
+import commands.ExamineCommand;
 import commands.ICommand;
-import commands.*;
+import commands.InventoryCommand;
+import commands.LookCommand;
+import commands.MoveCommand;
+import commands.TakeCommand;
+import commands.UseCommand;
 
+/**
+ * The {@code GameCommandFinder} class is responsible for parsing and retrieving the correct
+ * commands based on the user input.
+ */
 public class GameCommandFinder {
   private final Appendable output;
 
+  /**
+   * Constructs a {@code GameCommandFinder} class with the given output destination.
+   * @param output the {@code Appendable} object where command outputs will be written.
+   */
   public GameCommandFinder(Appendable output) {
     this.output = output;
   }
 
+  /**
+   * Splits the user command into an action and a noun, and matches the action to the corresponding
+   * {@code ICommand} instance.
+   * @param command : the user input string
+   * @return an instance of {@code ICommand} corresponding to the user input
+   * @throws IllegalArgumentException if the input is null or empty
+   */
   public ICommand getCommand(String command) throws IllegalArgumentException {
     if (command == null || command.isEmpty()) {
       throw new IllegalArgumentException("Input cannot be null or empty!");
@@ -36,6 +58,14 @@ public class GameCommandFinder {
     };
   }
 
+  /**
+   * Helper functions that splits the user input string into a verb and a noun.
+   * @param command user input string
+   * @return a list of string with two elements
+   *          - first element is the action
+   *          - second element is the noun
+   *         If the command only has one word then the array will only have one element.
+   */
   private List<String> splitCommand(String command) {
     String[] splitCommand = command.split(" ", 2);
     return Arrays.asList(splitCommand);
--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -5,30 +5,51 @@ import java.io.IOException;
 import commands.ICommand;
 import model.IGameModel;
 
+/**
+ * The {@code GameController} class manages the flow of the game.
+ * It handles the user input, the command processing, and interacting with the game model.
+ */
 public class GameController implements IController {
   private final IGameModel model;
   private final Readable input;
   private final Appendable output;
 
-
+  /**
+   * Constructs a {@code GameController} with the specified game model,
+   * input source and output destination.
+   * @param model : game model
+   * @param input : input source
+   * @param output : output destination
+   */
   public GameController(IGameModel model, Readable input, Appendable output) {
     this.model = model;
     this.input = input;
     this.output = output;
   }
 
+  /**
+   * Starts the game loop, continues prompting the user for input, getting the appropriate commands
+   * and updating the game state until the user quits the game.
+   * @throws IOException if an I/O error occurs while writing output
+   */
   @Override
   public void go() throws IOException {
+    // Display initial game state
     this.output.append(model.look());
+
+    // Initialize input reader and command processor
     GameInputReader inputReader = new GameInputReader(this.input, this.output);
     GameCommandFinder commandFinder = new GameCommandFinder(this.output);
     ICommand associatedCommand;
 
+    // Read string input from user
     String userInput = inputReader.readInput();
-    while(!userInput.equalsIgnoreCase("Q")) {
-      associatedCommand = commandFinder.getCommand(userInput);
-      associatedCommand.execute(this.model);
-      userInput = inputReader.readInput();
+
+    // Continuous game loop until player quits
+    while (!userInput.equalsIgnoreCase("Q")) {
+      associatedCommand = commandFinder.getCommand(userInput); // find command associated to input
+      associatedCommand.execute(this.model); // executes command
+      userInput = inputReader.readInput(); // prompt user for next command
     }
   }
 }
