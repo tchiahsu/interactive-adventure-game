d92a4cce8390ea4d2f7225c4250a4770d54bf02b
Bhoomika <bhoomigupta803@gmail.com>
2025-03-28 23:53:46 -0400

    item class method

--- test/model/ItemTest.java
+++ test/model/ItemTest.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Class that tests the method of the Item class.
+ */
 class ItemTest {
 
   Item i1;
@@ -24,8 +27,87 @@ class ItemTest {
     i1 = gameData.getItem("House Key");
   }
 
+  /**
+   * Tests the getName method of the Item class.
+   */
   @Test
   void testGetName() {
-    assertEquals("House Key", i1.getName());
+    assertEquals("HOUSE KEY", i1.getName());
   }
+
+  /**
+   * Tests the setName method of the Item class.
+   */
+  @Test
+  void testSetName() {
+    i1.setName("CAR KEY");
+    assertEquals("CAR KEY", i1.getName());
+  }
+
+  /**
+   * Tests the getName method of the Item class.
+   */
+  @Test
+  void testGetDescription() {
+    assertEquals("A simple old key that opens the front door of the house. "
+            + "It's engraved with the number '1'.", i1.getDescription());
+  }
+
+  /**
+   * Tests the getName method of the Item class.
+   */
+  @Test
+  void testGetPicture() {
+    assertEquals(null, i1.getPicture());
+  }
+
+  /**
+   * Tests the getWeight method of the Item class.
+   */
+  @Test
+  void testGetWeight() {
+    assertEquals(1, i1.getWeight());
+  }
+
+  /**
+   * Test the getMaxUses method of the Item class.
+   */
+  @Test
+  void testGetMaxUses() {
+    assertEquals(1,i1.getMaxUses());
+  }
+
+  /**
+   * Test the getUsesRemaining method of the Item class.
+   */
+  @Test
+  void testGetUsesRemaining() {
+    assertEquals(1,i1.getUsesRemaining());
+  }
+
+  /**
+   * Test the getValue method of the Item class.
+   */
+  @Test
+  void testGetValue() {
+    assertEquals(10,i1.getValue());
+  }
+
+  /**
+   * Test the getWhenUsedDescription method of the Item class.
+   */
+  @Test
+  void testGetWhenUsedDescription() {
+    assertEquals("You unlock the front door with the house key. It creaks as it opens.",i1.getWhenUsedDescription());
+  }
+
+  /**
+   * Tests the reduceUse method of the Item class.
+   */
+  @Test
+  void testReduceUse() {
+    i1.reduceUse();
+    assertEquals(0,i1.getUsesRemaining());
+  }
+
 }
