90274a81601e9e5fae619f355b286bf8dc8d5cf3
Tony Hsu <hsutai.c@gmail.com>
2025-03-30 21:30:04 -0400

    add null assertion

--- test/model/GameInfoTest.java
+++ test/model/GameInfoTest.java
@@ -2,6 +2,7 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
