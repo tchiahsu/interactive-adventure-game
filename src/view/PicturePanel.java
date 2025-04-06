b6f0154bffbc8c598bf4b2dd6b4e137e5556c427
Harrison Pham <hpham1997@gmail.com>
2025-04-06 18:36:57 -0400

    write PicturePanel class

--- src/view/PicturePanel.java
+++ src/view/PicturePanel.java
@@ -8,39 +8,88 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+/**
+ * A class representing the description panel in the graphical view of the game.
+ */
 public class PicturePanel extends JPanel {
+  // Data fields
+  // Add extra spaces to the room name as padding
+  private static final String NAME_PADDING = "   ";
+  private static final Color TEXT_COLOR = new Color(40, 54, 24);
+  private static final Color ROOM_PANEL_COLOR = new Color(212, 212, 212);
+
   private JPanel roomPanel;
-  BufferedImage image;
+  private JLabel roomLabel;
   private JLabel pictureLabel;
-  private static final Color MAIN_COLOR = new Color(40, 54, 24);
+  BufferedImage image;
 
-  public PicturePanel(String roomName, String picturePath) throws IOException {
-//    this.setLayout(new BorderLayout());
-//    BufferedImage image = ImageIO.read(new File(picturePath));
-//    this.pictureLabel = new JLabel(new ImageIcon(image));
-//    this.add(this.pictureLabel, BorderLayout.CENTER);
-//
-//    this.roomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-//    this.roomPanel.setBackground(MAIN_COLOR);
-//    this.add(this.roomPanel, BorderLayout.NORTH);
-
+  public PicturePanel(String roomName, String picturePath) {
     this.setLayout(new GridBagLayout());
-  }
+    GridBagConstraints gbc = new GridBagConstraints();
 
-  private void setPicture(String picturePath) throws IOException {
-    BufferedImage image = ImageIO.read(new File(picturePath));
-    pictureLabel.setIcon(new ImageIcon(image));
-  }
+    this.roomPanel = new JPanel();
+    this.roomPanel.setBackground(ROOM_PANEL_COLOR);
+    this.roomLabel = new JLabel(NAME_PADDING + roomName + NAME_PADDING);
+    Font font = getPanelFont().deriveFont(Font.BOLD, 30);
+    this.roomLabel.setFont(font);
+    this.roomLabel.setForeground(TEXT_COLOR);
+    roomPanel.add(roomLabel);
 
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.anchor = GridBagConstraints.NORTHWEST;
+    this.add(roomPanel, gbc);
+
+    try {
+      image = ImageIO.read(new File(picturePath));
+      this.pictureLabel = new JLabel(new ImageIcon(image));
+    } catch (IOException e) {
+      this.pictureLabel = new JLabel("No picture found");
+    }
+    gbc.gridy = 1;
+    gbc.weightx = 1;
+    gbc.weighty = 1;
+    gbc.fill = GridBagConstraints.BOTH;
+    this.add(pictureLabel, gbc);
+  }
 
   public void updatePicturePanel(String roomName, String picturePath) {
+    roomLabel.setText(NAME_PADDING + roomName + NAME_PADDING);
 
+    try {
+      image = ImageIO.read(new File(picturePath));
+      this.pictureLabel.setIcon(new ImageIcon(image));
+    } catch (IOException e) {
+      this.pictureLabel.setIcon(null);
+      this.pictureLabel.setText("No picture found");
+    }
   }
 
-  public static void main(String[] args) throws IOException {
+  /**
+   * Gets the Aharoni font for the panel.
+   *
+   * @return Aharoni font if the path to the file exists, Arial font otherwise.
+   */
+  private Font getPanelFont() {
+    File fontFile = new File("src/data/ahronbd.ttf");
+    Font font;
+    try {
+      font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+    } catch (Exception e) {
+      font = new Font("arial", Font.PLAIN, 14);
+    }
+
+    return font;
+  }
+
+  /**
+   * For testing.
+   */
+  public static void main(String[] args) {
     JFrame frame = new JFrame();
     frame.setLayout(new BorderLayout());
     PicturePanel pp = new PicturePanel("Hallway 1", "src/data/images/courtyard.png");
+//    pp.updatePicturePanel("Test", "src/data/images/bridge.png");
     frame.add(pp, BorderLayout.CENTER);
     frame.setVisible(true);
   }
