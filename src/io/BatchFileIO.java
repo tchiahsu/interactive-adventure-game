c0f329512939cb0df8ac4f12990a925c3da77531
Harrison Pham <hpham1997@gmail.com>
2025-04-07 16:28:40 -0400

    add batchConsoleIO class

---
+++ src/io/BatchFileIO.java
@@ -0,0 +1,28 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BatchFileIO implements IOHandler {
+  private final BufferedReader reader;
+  private final BufferedWriter writer;
+
+  public BatchFileIO(String sourceFile, String targetFile) throws IOException {
+    this.reader = new BufferedReader(new FileReader(sourceFile));
+    this.writer = new BufferedWriter(new FileWriter(targetFile));
+  }
+
+  @Override
+  public String read() throws IOException {
+    return reader.readLine();
+  }
+
+  @Override
+  public void write(String s) throws IOException {
+    writer.write(s);
+    writer.flush();
+  }
+}
